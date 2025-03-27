f3358bcc6a0b03f184db6bcf6622f14b17f9544e
MarkSemaan <mark.h.semaan@gmail.com>
2025-03-27 17:33:06 +0200

    Fixed sentence on comment

--- fixing_class_errors.py
+++ fixing_class_errors.py
@@ -1,5 +1,5 @@
 #First, Class Student did not inherit name (add super)
-#Second, dsplay grade won't output format since there's no f in the print
+#Second, dsplay grade won't output format since there's no f in the print,  and there's no self argument.
 #Third, in display grade, it's self.name and not name
 #Fourth, in display grade, it's self.grade and not grade
 #Fith, we should create a student object, not a person object.
